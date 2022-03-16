669d593682e8eadcaaa3720c66a2299b6fd9d98d
adriano.souza <adriano.souza@zpesystems.com>
2022-03-16 17:48:41 -0300

    Enhancing NewTransactionModal component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from 'react-modal';
 
+import { Container } from './styles';
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -8,12 +10,34 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
   return (
-    
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName='react-modal-overlay'
+      className="react-modal-content"
     >
-      <h2>Add New Transaction</h2>
+      <Container>
+        <h2>Add New Transaction</h2>
+
+        <input 
+          placeholder='Title'
+        />
+
+        <input 
+          type="number"
+          placeholder='Amount'
+        />
+
+        <input 
+          placeholder='Category'
+        />
+
+        <button type="submit">
+          Save
+        </button>
+
+
+      </Container>
     </Modal>
   )
 }
