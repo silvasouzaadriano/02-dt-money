ed99d5457db36f58419067cd22737f3fb3e08083
adriano.souza <adriano.souza@zpesystems.com>
2022-03-16 23:42:21 -0300

    Saving transaction in API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,8 @@ import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
+import { api } from '../../services/api'
+
 import { Container, TransactionTypeContainer, TransactionTypeRadioBox } from './styles';
 
 interface NewTransactionModalProps {
@@ -20,12 +22,15 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    console.log(
+    const data = {
       title,
       amount,
       category,
       transactionType
-    )
+    }
+
+    api.post('/transactions', data)
+
   }
 
   return (
